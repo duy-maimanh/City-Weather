d2024c08add69a27b97348c67ddfae2e8f3f9bc5
Duy Mai <duymmpd95@gmail.com>
2022-09-30 17:29:04 +0700

    fix bug statusbar background not hide

--- app/src/main/java/com/mmd/cityweather/common/MainActivity.kt
+++ app/src/main/java/com/mmd/cityweather/common/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mmd.cityweather.common
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import com.mmd.cityweather.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,13 +14,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        // Set it to hide status bar background.
+        // use it for android api >= 30. check here https://stackoverflow.com/questions/50142064/how-to-have-a-transparent-status-bar-but-leave-navigation-bar-opaque
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Set it to hide status bar background.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
     }
 }
