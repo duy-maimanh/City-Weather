3b7029da8e2168dd73c04fa6993eb57f96e5a963
Duy Mai <duymmpd95@gmail.com>
2022-10-28 17:12:07 +0700

    add toolbar for city management

--- app/src/main/java/com/mmd/cityweather/citiesmanage/presentation/CitiesManageFragment.kt
+++ app/src/main/java/com/mmd/cityweather/citiesmanage/presentation/CitiesManageFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mmd.cityweather.R
+import com.mmd.cityweather.common.MainActivity
 import com.mmd.cityweather.databinding.FragmentCitiesManageBinding
 
 class CitiesManageFragment : Fragment() {
@@ -16,7 +18,9 @@ class CitiesManageFragment : Fragment() {
     private lateinit var citiesAdapter: CitiesManageAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentCitiesManageBinding.inflate(
@@ -31,9 +35,24 @@ class CitiesManageFragment : Fragment() {
     }
 
     private fun initView() {
+        val statusBarHeightId =
+            resources.getIdentifier("status_bar_height", "dimen", "android")
+        val statusBarHeight = resources.getDimensionPixelSize(statusBarHeightId)
+        (binding.toolbarManageCities.layoutParams as? ViewGroup.MarginLayoutParams)?.topMargin =
+            statusBarHeight
+
+        binding.toolbarManageCities.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
         citiesAdapter = CitiesManageAdapter()
-        val decoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
-        ContextCompat.getDrawable(requireContext(), R.drawable.manage_cities_divider)?.let {
+        val decoration = DividerItemDecoration(
+            requireContext(),
+            DividerItemDecoration.VERTICAL
+        )
+        ContextCompat.getDrawable(
+            requireContext(),
+            R.drawable.manage_cities_divider
+        )?.let {
             decoration.setDrawable(it)
         }
         with(binding.recyclerviewCities) {
